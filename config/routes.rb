85582574eac09be6997993eb3db3aa713defc7f7
Rafid Hoda <rafidhoda@gmail.com>
2021-09-19 16:29:59 +0000

    Add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
-  resources :products
+  resources :products do
+    resources :reviews
+  end
   devise_for :users
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_155012) do
+ActiveRecord::Schema.define(version: 2021_09_19_161028) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2021_09_19_155012) do
     t.integer "items_per_order"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "rating"
+    t.integer "product_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +47,6 @@ ActiveRecord::Schema.define(version: 2021_09_19_155012) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
