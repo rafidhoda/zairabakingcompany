a61c54073a18b8ed592c517bcccd447f6b0a2fde
Rafid Hoda <rafidhoda@gmail.com>
2021-09-19 16:08:47 +0000

    Add user profiles and view profile link

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'contact' => 'pages#contact'
   resources :products
   devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
